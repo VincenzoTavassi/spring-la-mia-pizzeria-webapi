b1dd3960ac6fa4ffb82b3b0c149307f4e9ebab6f
VincenzoTavassi <info@egadivacanze.it>
2023-06-30 15:42:00 +0200

    +ingredients delete, form

--- src/main/java/org/java/excercises/pizzeria/controllers/IngredientController.java
+++ src/main/java/org/java/excercises/pizzeria/controllers/IngredientController.java
@@ -3,11 +3,16 @@ package org.java.excercises.pizzeria.controllers;
 import org.java.excercises.pizzeria.models.Ingredient;
 import org.java.excercises.pizzeria.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class IngredientController {
@@ -21,4 +26,13 @@ public class IngredientController {
         model.addAttribute("ingredients", ingredients);
         return "ingredients/index";
     }
+
+    @PostMapping("/ingredients/delete")
+    public String delete(@RequestParam("ingredientId") Integer id, Model model) {
+        Optional<Ingredient> ingredient = ingredientRepository.findById(id);
+        if (ingredient.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        ingredientRepository.deleteById(id);
+        return "redirect:/ingredients";
+    }
+
 }
